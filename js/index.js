09706de001a911c40478c68fbc45a07f8e40dfcb
Teja Rajam <34567797+rtejasiddhartha@users.noreply.github.com>
2025-05-22 14:07:50 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,161 +1,121 @@
-$(document).ready( function() {
-var c = 0;
-var h = 0;
-var rightClick = 0;
-var n;
-    
-//This code will run after your page loads
-$("#score").html('Score : '+c+'<br> High Score : '+h);
-    //This code will run after your page loads
+$(document).ready(function () {
+  let score = 0;
+  let highScore = 0;
+  let clickCount = 0;
+  let yetiIndex;
 
-createGame();
-createMound();    
-//Shuffling Random Images//
-RandomImage();    
-    
-$(".FindThePenguin").on('click', function (event) {
-/*$("<audio></audio>").attr({
-'src': 'media/Penguinpop.wav',
-'volume': 1,
-'autoplay': 'autoplay'
-}).appendTo("body");*/
-setGame($(this));
-});
-    
-function createGame() {
+  const mounds = $(".FindThePenguin");
 
-  $("#message").css("visibility","hidden");
-  $("#title").on("click", function() {
-    $(this).fadeOut(1000, createMound());
-  });
-  // or on keypress
-  $("body").on("keypress", function(event) {
-    if (event.which === 13) {
-      $("#title").fadeOut(1000, createMound());
-    }
-  });
-}
+  function updateScore() {
+    animateScore();
+    $("#score").html(`Score: ${score}<br>High Score: ${highScore}`);
+  }
 
-function createMound() {
+  function animateScore() {
+    $("#score").fadeOut(100).fadeIn(100);
+  }
 
-  // Add ice mounds to the game
-  mound = $(".FindThePenguin");
-  // Add mound hover effect
-  mound.on("mouseenter", function() {
-    $(this).addClass("moundHover");
-  });
-  mound.on("mouseleave", function() {
-    $(this).removeClass("moundHover");
-  });
-}   
-//Updating Score//
-function setGame(penguin) {    
-rightClick++;
-//console.log(rightClick); 
-penguin.css("pointer-events","none");    
-var num = penguin.attr('id');
-var Char = num.substr(num.length - 1);
-if (penguin.hasClass("FindThePenguin yetti"))
-{
-$("<audio></audio>").attr({
-'src': 'media/Yetipop.wav',
-'volume': 1,
-'autoplay': 'autoplay'
-}).appendTo("body");
-      //alert("Game Over. Want to Play Next Game Click Ok.");     
-penguin.css('background-image', 'url(images/yeti.png)');
-$("#score").html('Score: '+c+'<br> High Score : '+ h);
+  function playSound(src) {
+    const audio = new Audio(src);
+    audio.volume = 1;
+    audio.play();
+  }
 
-  penguin.on("mouseleave", function() {
-    $(this).removeClass("yetti"); 
-  });   
-   
-endGame(0); 
-}
-else
-{
-$("<audio></audio>").attr({
-'src': 'media/Penguinpop.wav',
-'volume': 1,
-'autoplay': 'autoplay'
-}).appendTo("body");    
-c = c + 10;               
-if(Char=='9')
-    {
-      //console.log(n.toString());    
-      penguin.css('background-image', 'url(images/penguin_' + n.toString() + '.png)');   
-    }
-else{
-    penguin.css('background-image', 'url(images/penguin_' + Char + '.png)'); 
-}    
-$("#score").html('Score : '+c+'<br> High Score : '+h);
-   if(rightClick == 8)
-       {
-           endGame(1);
-       }
-}
-}
-    
-function endGame(winValue) {
-            if(c>h)
-            {
-                h=c;
-        $("#score").html('Your Score : '+c+'<br> High Score : '+h);
-                //console.log("Score : "+c+" Highscore : "+h);
-            }
-  // fade rest of the game
-  $("#gameholder").fadeOut(3000, function() {
-      $("#message").css("visibility","hidden");
-    if (winValue) {    
-      $("#message").text("Congratulation, You Win !!! \n Click here to play again");
-    } else { 
-      //alert("Yaaaarrrr ! Its Yetiii");    
-      $("#message").text("Yeti Ate You!!! \n Click here to play again");
-    }
-      
-    $("#message").css("visibility","visible"); 
-    $("#message").css("display", "inline-block");
-    $("#message").on("click", function() {
-      reset();
+  function placeYeti() {
+    mounds.each(function () {
+      $(this).data("role", "penguin").removeClass("yetti");
     });
-    $("body").on("keypress", function(event) {
-      if (event.which === 13) {
-        reset();
+
+    yetiIndex = Math.floor(Math.random() * 9);
+    mounds.eq(yetiIndex).data("role", "yeti").addClass("yetti");
+  }
+
+  function setGame() {
+    // Prevent duplicate click handlers
+    mounds.off("click");
+
+    mounds.each(function (index) {
+      $(this)
+        .data("index", index + 1)
+        .data("role", "penguin")
+        .css("pointer-events", "auto");
+    });
+
+    mounds.on("click", function () {
+      const role = $(this).data("role");
+      $(this).css("pointer-events", "none");
+      clickCount++;
+
+      if (role === "yeti") {
+        $(this).css("background-image", "url(images/yeti.png)");
+        playSound("media/Yetipop.wav");
+        if (navigator.vibrate) navigator.vibrate(200);
+        endGame(false);
+      } else {
+        const i = $(this).data("index");
+        let imgIndex = (i - 1) % 8 + 1; // fallback for missing penguin_9.png
+        $(this).css("background-image", `url(images/penguin_${imgIndex}.png)`);
+        playSound("media/Penguinpop.wav");
+
+        score += 10;
+        updateScore();
+        if (clickCount === 8) endGame(true);
       }
     });
-          
+  }
+
+  function endGame(win) {
+    if (score > highScore) highScore = score;
+    updateScore();
+
+    $("#gameholder").fadeOut(800, function () {
+      $("#message")
+        .text(win ? "🎉 You Win! Tap to play again." : "😱 Yeti Found You! Tap to retry.")
+        .css({
+          display: "inline-block",
+          visibility: "visible",
+          backgroundColor: win ? "#d4edda" : "#f8d7da"
+        });
+    });
+  }
+
+  function resetGame() {
+    score = 0;
+    clickCount = 0;
+
+    mounds.each(function (index) {
+      $(this)
+        .data("index", index + 1)
+        .data("role", "penguin")
+        .removeClass("yetti")
+        .css({
+          "pointer-events": "auto",
+          "visibility": "visible",
+          "background-image": `url(images/mound_${index + 1}.png)`
+        });
+    });
+
+    $("#gameholder").show();
+    $("#message").hide();
+    updateScore();
+    placeYeti();
+    setGame(); // reattach click handler safely
+  }
+
+  $("#message").on("click", resetGame);
+  $("body").on("keypress", function (e) {
+    if (e.which === 13 && $("#message").is(":visible")) resetGame();
   });
-      
-}    
 
-function reset() {
-    c=0;
-    rightClick = 0;
-    $("#score").html('Your Score : '+c+'<br> High Score : '+h);
-    //console.log("Score : "+c+" Highscore : "+h);    
-  //make message disappear
-  $("#message").css("display", "none");
-  // create ice mounds again
-  createMound();
-  //make gameholder display again
-  $("#gameholder").css("display", "block");
-  $(".FindThePenguin").removeClass("yetti");    
-  $(".FindThePenguin").removeAttr("style");
-  $(".FindThePenguin").css("visibility", "visible");    
-  // enable play game once more
-  createGame();
+  // Ensure mounds are visible with background at page load
+  mounds.each(function (index) {
+    $(this)
+      .data("index", index + 1)
+      .data("role", "penguin")
+      .css("background-image", `url(images/mound_${index + 1}.png)`);
+  });
 
-          $( "div[id*='yeti']" ).attr('id','penguin'+n);
-
-  RandomImage();    
-}
-        
-function RandomImage() {
-
-n = Math.floor((Math.random() * 9)+1);
-//console.log("randon number : "+n);
-$("#penguin" + n).addClass('yetti');
-$("#penguin" + n).attr('id', 'yeti');
-$('#penguin9').css('background-image', 'url(images/mound_' + n.toString() + '.png)');
-}
+  setGame();
+  updateScore();
+  placeYeti();
 });
